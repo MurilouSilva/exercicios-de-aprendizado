18b88c6ec8fba8f90b66d9fbaffd737d979a2c57
Murilo Silva <murilou6@gmail.com>
2026-09-21 19:15:14 -0300

    Update exercicio 5.1 .C

--- exercicio 5.1 .C
+++ exercicio 5.1 .C
@@ -6,17 +6,17 @@ int main(int argc, char const *argv[])
     printf("Calculadora Aritmetica\n\n");
     printf("digite um numero\n");
     float num1;
-    scanf ("%f", &num1);
+    scanf("%f", &num1);
     printf("digite o segundo numero\n\n");
     float num2;
-    scanf ("%f", &num2);
+    scanf("%f", &num2);
     printf("agora, digite um operador(adicao,subtracao,divisao,multiplicacao)\n");
     char operador[20];
     scanf("%s", operador);
     if (strcmp(operador, "adicao") == 0)
     {
         printf("Resultado:%.2f\n", num1 + num2);
-    } 
+    }
     else if (strcmp(operador, "subtracao") == 0)
     {
         printf("Resultado:%.2f\n", num1 - num2);
